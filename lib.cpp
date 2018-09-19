a5c974159e9ea19d8ca79ab9cd434b95240ce36d
Thibault RAMBOUR <thibaultrambour3@gmail.com>
2018-09-19 11:38:19 +0200

    creation premier programme compilable

---
+++ lib.cpp
@@ -0,0 +1,6 @@
+#include "lib.h"
+#include <iostream>
+
+int calcul::somme (int nombre_1,int nombre_2){
+return(nombre_1+nombre_2);
+}
---
+++ lib.h
@@ -0,0 +1,6 @@
+class calcul
+{
+public:
+
+int somme (int nombre_1,int nombre_2);
+};
