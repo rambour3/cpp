7b2c852871f06bb108d5008ba8409c17add7304d
Thibault RAMBOUR <thibaultrambour3@gmail.com>
2018-09-26 12:30:36 +0200

    commentaire

--- lib.h
+++ lib.h
@@ -1,10 +1,27 @@
+/*!\file lib.h
+*\brief 
+*\autor
+*\cversion
+*
+*
+*/
 
+/*!\class calcul
+*\brief
+*voici une classe qui permets de faire des calcules 
+*/
 class calcul
 {
 public :
-
+/*! \brief 
+*\param a  nombre 1 quelconque 
+*\param b nombre 2 quelquonqu
+*\return des ints
+*/
 int somme (int nombre_1,int nombre_2);
-int sous (int nombre_1,int nombre_2);
+
+int sous (int nombre_1,int nombre_2)
+;
 int mult(int nombre_1,int nombre_2);
 
 };
--- main.cpp
+++ main.cpp
@@ -7,14 +7,11 @@ using namespace std;
 int main() {
 int a=0;
 int b=2;
-bool h=0;
 
 calcul c;
 
 cout<<c.somme(a,b)<<endl;
 cout<<c.sous(a,b)<<endl;
 cout<<c.mult(a,b)<<endl;
-cout<<
 
-return 0;
 }
